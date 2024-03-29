7d15f9b8236b3f3b6bae06471ad64633f85c0d43
Lexia Help <163201207+Anti-iReady@users.noreply.github.com>
2024-03-29 09:07:41 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const windows = [];
+
+function openWindow() {
+  const win = window.open("", "", "width=200,height=200");
+  win.document.write(`<audio autoplay><source src="rickroll.mp3" type="audio/mpeg"></audio><img src="rickroll.gif" style="position: absolute; top: 0; left: 0;">`);
+  windows.push(win);
+  moveWindow(win);
+}
+
+function moveWindow(win) {
+  const x = Math.floor(Math.random() * window.innerWidth);
+  const y = Math.floor(Math.random() * window.innerHeight);
+  win.moveTo(x, y);
+  setTimeout(() => moveWindow(win), 100);
+}
+
+for (let i = 0; i < 6; i++) {
+  openWindow();
+}
+
+window.onbeforeunload = () => {
+  windows.forEach(win => win.close());
+};
+
+chrome.app.runtime.onLaunched.addListener(() => {
+  for (let i = 0; i < 6; i++) {
+    openWindow();
+  }
+});
